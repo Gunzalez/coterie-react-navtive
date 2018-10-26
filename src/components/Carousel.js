9d7c1ba8adc6f954a566e8ca14ba7d611aeb1514
Segun Konibire <gunzalez@hotmail.com>
2018-10-26 19:04:01 +0100

    implements pagination

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,6 +7,7 @@ import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 import PotSlide from './PotSlide';
+import utils from "../utils";
 
 const horizontalMargin = 0;
 const slideWidth = 280;
@@ -29,15 +30,42 @@ class MyCarousel extends Component {
         );
     }
 
+    state = {
+        entries: this.props.pots,
+        activeSlide: 1
+    };
+
+    get pagination () {
+        const { entries, activeSlide } = this.state;
+        return (
+            <Pagination
+                dotsLength={entries.length}
+                activeDotIndex={activeSlide}
+                containerStyle={styles.pagination}
+                dotStyle={styles.dot}
+                inactiveDotStyle={{
+                    // Define styles for inactive dots here
+                }}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.9}
+            />
+        );
+    }
+
     render () {
         return (
-            <Carousel
-                layout={'default'}
-                data={this.props.pots}
-                renderItem={this._renderItem}
-                sliderWidth={sliderWidth}
-                itemWidth={slideWidth}
-            />
+            <View>
+                <Carousel
+                    layout={'default'}
+                    data={this.state.entries}
+                    renderItem={this._renderItem}
+                    sliderWidth={sliderWidth}
+                    itemWidth={slideWidth}
+                    onSnapToItem={(index) => this.setState({ activeSlide: index }) }
+                    firstItem={this.state.activeSlide}
+                />
+                { this.pagination }
+            </View>
         );
     }
 }
@@ -51,6 +79,15 @@ const styles = StyleSheet.create({
     slideInnerContainer: {
         width: slideWidth,
         flex: 1
+    },
+    pagination: {
+
+    },
+    dot: {
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        backgroundColor: utils.colours.purple
     }
 });
 
