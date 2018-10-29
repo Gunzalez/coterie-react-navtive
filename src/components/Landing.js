30eb738a65fae2ab1b1fed754168ae1dcabb59cd
Segun Konibire <gunzalez@hotmail.com>
2018-10-29 19:14:04 +0000

    sample content for detail screen

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -28,37 +28,73 @@ class Detail extends Component {
 
     render() {
 
-        const { potId, title } = this.state.potDetail;
+        const { potId,
+            title = 'Create a new pot',
+            amount = '-',
+            participants = '-',
+            status = 0,
+            round = '-',
+            current = '-',
+            next = 'Trump'} = this.state.potDetail;
+
+        const curPotValue = parseInt(current) * parseInt(amount);
+        const totPotValue = parseInt(participants) * parseInt(amount);
 
         return (
-            <View style={[ styles.slide ]}>
+            <View style={[ styles.container ]}>
                 <Text style={[ styles.title ]}>Detail screen</Text>
-                <Text>pot Id :{ potId }</Text>
-                <Text>Title: { title }</Text>
-                <TouchableOpacity onPress={this.handlePress}>
-                    <Text>Back</Text>
+                <Text style={styles.heading}>Pot/Form details</Text>
+                <Text>Title :{ title } (editable before payment)</Text>
+                <Text>Amount: £{ amount } (editable before payment)</Text>
+                <Text>No. of Participants: { participants } (editable before payment)</Text>
+                <Text>---</Text>
+                <Text style={styles.heading}>Meta</Text>
+                <Text>Pot Status: { status } (derived)</Text>
+                <Text>Current round: { round } (derived)</Text>
+                <Text>Next to get paid: { next } (derived)</Text>
+                <Text>Current pot value: £{ curPotValue } (derived)</Text>
+                <Text>Total pot value: { totPotValue } (derived)</Text>
+                <Text>---</Text>
+                <Text style={styles.heading}>Actions</Text>
+                <Text>- Delete a pot (may be disabled)</Text>
+                <Text>- Save a Pot (may be disabled)</Text>
+                <Text>- Manage payment/collection (click on Participant)</Text>
+                <Text>- Clear Pot/Form (may be disabled)</Text>
+                <Text>- Clone Pot (may be disabled)</Text>
+                <Text>- Add/Remove Participants (may be disabled)</Text>
+                <Text>- Re-order Participants (may be disabled)</Text>
+                <Text>---</Text>
+                <Button title="Add/Remove Participants" onPress={this.showParticipants} />
+                <TouchableOpacity style={styles.back} onPress={this.handlePress}>
+                    <Text style={styles.backText}>Back to Pots</Text>
                 </TouchableOpacity>
-                <Button title="Add Participants" onPress={this.showParticipants} />
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    slide: {
-        padding: 20,
-        borderRadius: 5
-    },
-    full: {
-        backgroundColor: utils.colours.purple
+    container: {
+        padding: 20
     },
     title: {
         fontSize: 20,
         paddingBottom: 10
     },
-    text: {
-        color: utils.colours.white
-
+    heading:{
+        fontSize: 18,
+        paddingTop: 15
+    },
+    back: {
+        backgroundColor: utils.colours.purple,
+        marginTop: 50,
+        padding: 12,
+        borderRadius: 5
+    },
+    backText: {
+        color: utils.colours.white,
+        fontSize: 18,
+        textAlign: 'center'
     }
 });
 
